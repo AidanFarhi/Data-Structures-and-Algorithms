1e4606f044f2caa24e8e305bc5aa7cb9d7f4a058
Aidan Farhi <aidanfarhi@Aidans-MacBook-Air.local>
2020-09-17 05:57:09 -0400

    arrays added

---
+++ arrays.py
@@ -0,0 +1,54 @@
+"""
+
+- Array Data Type -
+
+Positives:
+- Adding, Searching is a fast operation
+- Random access is possible
+- Can construct multidimensional arrays (matrix)
+
+Negatives:
+- Have to know the size of array at compile time
+- Not able to store items with different value types (Java, C++, C)
+- If array is full, you have to copy the all
+  the contents of the array first, which takes O(N).
+
+Applications:
+- lookup tables
+- hash tables
+- heaps
+
+Operations:
+- add(value) O(1) add to next unoccupied index
+- insert(value, index) O(N) we have to shift all the items first
+- removeLast() O(1) remove last item
+- remove(value) O(N) have to shift all items after removal
+
+"""
+
+# basic array construction
+array = [10, 3, 7, 5]
+
+# random indexing: indexing starts with 0
+print(array[2])
+
+# get all items
+print(array[:])
+
+# get first 3 items
+print(array[0:3])
+
+# get all except last one
+print(array[:-1])
+
+# update a value
+array[3] = 100
+
+# linear search O(N) (finding max item)
+maximum = array[0]
+
+for num in array:
+    if num > maximum:
+        maximum = num
+
+print(maximum)

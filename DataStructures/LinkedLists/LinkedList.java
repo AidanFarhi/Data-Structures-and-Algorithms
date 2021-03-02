80d05cf8b43c48c9c964a161575f3de9dd31acd3
Aidan Farhi <aidanfarhi@Aidans-MacBook-Air.local>
2021-03-01 22:07:58 -0500

    added LL in java + python

---
+++ DataStructures/LinkedLists/LinkedList.java
@@ -0,0 +1,93 @@
+package DataStructures.LinkedLists;
+
+import java.util.NoSuchElementException;
+
+public class LinkedList {
+
+    Node head = null;
+    int size = 0;
+
+    private class Node {
+        
+        Object data;
+        Node next;
+        
+        public Node(Object data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void insertFront(Object data) {
+        size++;
+        if (this.head == null) this.head = new Node(data);
+        else {
+            Node newNode = new Node(data);
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+
+    public Object removeFront() {
+        if (head == null) throw new NoSuchElementException("List is empty.");
+        size--;
+        Object data = head.data;
+        head = head.next;
+        return data;
+    }
+
+    public void insertEnd(Object data) {
+        size++;
+        if (head == null) head = new Node(data);
+        else {
+            Node n = head;
+            while (n.next != null) n = n.next;
+            n.next = new Node(data);
+        }
+    }
+
+    public Object removeEnd() {
+        if (head == null) throw new NoSuchElementException("List is empty");
+        size--;
+        Node n = head;
+        Node prev = null;
+        while (n.next != null) { // iterate to end of list
+            prev = n;
+            n = n.next; 
+        }
+        Object data = n.data;
+        if (prev == null) head = null;  // head is node to remove
+        else prev.next = null;          // set last node to null
+        return data;
+    }
+
+    public void traverse() {
+        Node n = head;
+        while (n != null) {
+            System.out.println(n.data);
+            n = n.next;
+        }
+    }
+
+    // Test Client
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        ll.insertFront(300);
+        ll.insertFront(200);
+        ll.insertFront(100);
+        ll.insertEnd(400);
+        ll.insertEnd(500);
+        ll.insertEnd(600);
+        ll.traverse();
+        System.out.println("Size: " + String.valueOf(ll.getSize()));
+        System.out.println("-------");
+        ll.removeEnd();
+        ll.removeFront();
+        ll.traverse();
+        System.out.println("Size: " + String.valueOf(ll.getSize()));
+    }
+}
--- DataStructures/LinkedLists/LinkedList.py
+++ DataStructures/LinkedLists/LinkedList.py
@@ -1,44 +1,3 @@
-"""
-- Linked List -
-
-Positives:
-- Linked lists are dynamic (arrays have a fixed size).
-- It can allocate needed memory in run-time.
-- Efficient if we want to manipulate the first elements.
-- Easy to implement.
-- Can store items with different sizes (arrays assume every item same size).
-- Easier for linked list to grow organically.
-
-Negatives:
-- Wastes memory because of the references.
-- Nodes must be read in order, from the beginning because linked lists
-  have sequential access.
-- Difficulties arise in linked lists when it comes to reverse traversing.
-  Singly linked lists are extremely difficult to navigate backwards.
-- Doubly linked lists sacrifice by using extra memory for the ability to navigate backwards.
-
-Applications:
-- Stacks
-- Queues
-
-Operations:
-is_empty() -> O(1)
-get_size() -> O(1)
-value_at(index) -> O(N)
-push_front(item) -> O(1)
-pop_front() -> O(1)
-push_back(item) -> O(N)
-pop_back() -> O(N)
-peek_front() -> O(1)
-peek_back() -> O(N)
-insert_at() -> O(N)
-erase(index) -> O(N)
-reverse() -> O(N)
-remove_value(item) -> O(N)
-show_list() -> O(N)
-"""
-
-
 class Node:
 
     def __init__(self, data):
@@ -55,155 +14,74 @@ class LinkedList:
     def get_size(self):
         return self.size
 
-    def is_empty(self):
-        return self.size == 0
-
-    def value_at(self, index):
-        if index >= self.size:
-            raise IndexError('Index out of range')
-        if self.head:
-            i = 0
-            node = self.head
-            while i < index:
-                node = node.next
-                i += 1
-            return node.data
-
-    def push_front(self, item):
-        if self.head is not None:
-            new_node = Node(item)
+    # O(1) operation
+    def insert_start(self, data):
+        self.size += 1
+        if self.head is None:
+            self.head = Node(data)
+        else:
+            new_node = Node(data)
             new_node.next = self.head
             self.head = new_node
-        else:
-            self.head = Node(item)
-        self.size += 1
 
-    def pop_front(self):
-        if self.head is not None:
-            data = self.head.data
-            new_head = self.head.next
-            if new_head is not None:
-                self.head = new_head
-            else:
-                self.head = None
+    # O(1) operation
+    def remove_start(self):
+        if self.head is None:
+            return None
+        self.size -= 1
+        data = self.head.data
+        self.head = self.head.next
+        return data
+
+    # O(N) operation
+    def insert_end(self, data):
+        if self.head is None:
+            self.insert_start(data)
+        else:
+            self.size += 1
+            n = self.head
+            while n.next is not None:
+                n = n.next
+            n.next = Node(data)
+
+    # O(N) operation
+    def remove_end(self):
+        if self.head is None:
+            return
+        elif self.size == 1:
+            self.remove_start()
+        else:
             self.size -= 1
+            n = self.head
+            prev = None
+            while n.next is not None:
+                prev = n
+                n = n.next
+            data = n.data
+            prev.next = None
             return data
 
-    def push_back(self, item):
-        if self.head is not None:
-            new_node = Node(item)
-            node = self.head
-            while node.next:
-                node = node.next
-            node.next = new_node
-            self.size += 1
-        else:
-            self.head = Node(item)
-            self.size += 1
+    def traverse(self):
+        n = self.head
+        while n is not None:
+            print(n.data)
+            n = n.next
 
-    def pop_back(self):
-        if self.head is not None:
-            node = self.head
-            prev = None
-            while node.next:
-                prev = node
-                node = node.next
-            if prev is not None:
-                prev.next = None
-            else:
-                self.head = None
-            self.size -= 1
 
-    def peek_front(self):
-        return self.head.data
+def main():
 
-    def peek_back(self):
-        if self.head:
-            node = self.head
-            while node.next is not None:
-                node = node.next
-            return node.data
+    ll = LinkedList()
+    ll.insert_start(300)
+    ll.insert_start(200)
+    ll.insert_start(100)
+    ll.insert_end(400)
+    ll.insert_end(500)
+    ll.insert_end(600)
+    ll.traverse()
+    print('-------------')
+    ll.remove_end()
+    ll.remove_end()
+    ll.traverse()
 
-    def insert_at(self, index, item):
-        if index > self.size:
-            raise IndexError('Index out of range')
-        if index == self.size:
-            self.push_back(item)
-            return
-        if self.head:
-            new_node = Node(item)
-            i = 0
-            node = self.head
-            prev = None
-            while i < index:
-                prev = node
-                node = node.next
-                i += 1
-            if prev is not None:
-                prev.next = new_node
-                new_node.next = node
-            else:
-                new_node.next = self.head
-                self.head = new_node
-            self.size += 1
 
-    def erase(self, index):
-        if index > self.size:
-            raise IndexError('Index out of range')
-        if index == 0:
-            self.pop_front()
-            return
-        if self.head:
-            i = 0
-            node = self.head
-            prev = None
-            while i < index:
-                prev = node
-                node = node.next
-                i += 1
-            if prev is not None:
-                prev.next = node.next
-            else:
-                self.head = None
-            self.size -= 1
-
-    def reverse(self):
-        if self.head and self.head.next is not None:
-            node = self.head
-            prev = None
-            while node:
-                next_node = node.next
-                node.next = prev
-                prev = node
-                node = next_node
-            self.head = prev
-
-    def remove_value(self, item):
-        if self.head:
-            node = self.head
-            prev = None
-            while node:
-                if node.data == item:
-                    if prev is not None:
-                        prev.next = node.next
-                        self.size -= 1
-                        return
-                    else:
-                        if self.head.next is not None:
-                            self.head = self.head.next
-                        else:
-                            self.head = None
-                        self.size -= 1
-                prev = node
-                node = node.next
-
-    def show_list(self):
-        if self.head:
-            display = []
-            node = self.head
-            while node:
-                display.append(node.data)
-                node = node.next
-            return display
-        else:
-            return []
+main()
